e23bf5774b49a001ab68a2ffabc5c4f1ee4314b3
Benedikt Volkel <benedikt.volkel@cern.ch>
2022-04-06 16:52:58 +0200

    Add plot test and use common cwd

--- ci/pytest/test_plot.py
+++ ci/pytest/test_plot.py
@@ -1,8 +1,11 @@
 import sys
 from os.path import join, dirname, realpath
 import importlib.util
+from glob import glob
 
 from fast_plotting.config import read_config, configure_from_sources
+from fast_plotting.plot.utils import plot_auto, add_plot_for_each_source
+from fast_plotting.registry import read_from_config
 
 path = dirname(realpath(__file__))
 module_name = "common_test_utils"
@@ -13,24 +16,20 @@ spec.loader.exec_module(common_test_utils)
 
 from common_test_utils import make_root, change_test_dir
 
-JSON_TEST_FILE = join(path, "test.json")
-
-def test_read_sources(change_test_dir):
-    passed = True
-    try:
-        config = read_config(JSON_TEST_FILE)
-        config.print_sources()
-    except:
-        passed = False
-    assert passed
-
-
-def test_make_config_from_source(change_test_dir):
+def test_make_plot_from_source(change_test_dir):
     file_name = make_root()
     passed = True
+    plot_output_dir = "plots_output_test_make_plot_from_source"
     try:
         config = configure_from_sources((file_name,), ("label",))
-        config.write("config.json")
+        add_plot_for_each_source(config)
+        config.enable_plots("all")
+        read_from_config(config)
+        plot_auto(config, plot_output_dir)
+        plot_auto(config, plot_output_dir, True)
+        n_plots_config = len(config.get_plots())
+        n_plots_saved = len(glob(f"{plot_output_dir}/*.png"))
+        passed = n_plots_config + 1 == n_plots_saved
     except:
         passed = False
     assert passed

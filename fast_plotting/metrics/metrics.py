3db88eb98a9338a40511b37acb19579c52ae3125
Benedikt Volkel <benedikt.volkel@cern.ch>
2022-04-12 18:07:06 +0200

    More metrics dev

--- fast_plotting/metrics/metrics.py
+++ fast_plotting/metrics/metrics.py
@@ -20,6 +20,18 @@ def integral(data_wrapper):
     weights = np.outer(be_x, be_y).flatten()
     return np.dot(values.flatten(), weights)
 
+def _norm(data_wrapper):
+    if not data_wrapper:
+        return None
+    repr, dim = data_wrapper.preferred_representation()
+    if repr != datatypes.DATA_REPRESENTATION_HISTOGRAM:
+        return None
+    values, _ = data_wrapper.get_as_histogram()
+    return = values / integral(data_wrapper)
+
+def norm(data_wrapper):
+    return _norm(data_wrapper)
+
 # def rms(data_wrapper):
 #     repr, dim = data_wrapper.preferred_representation()
 #     if repr != datatypes.DATA_REPRESENTATION_HISTOGRAM:
@@ -52,6 +64,7 @@ def integral(data_wrapper):
     # return None
 
 def compare_relative(func, data_wrapper_1, data_wrapper_2):
+    """Given function, compare 2 sets of data"""
     den = func(data_wrapper_2)
     num = func(data_wrapper_1)
     if not den:
@@ -62,29 +75,38 @@ def compare_relative(func, data_wrapper_1, data_wrapper_2):
         return None
     return num / den, num, den
 
-def shape(data_wrapper_1, data_wrapper_2):
-    if data_wrapper_1.get_shape() != data_wrapper_2.get_shape():
-        METRICS_LOGGER.warning("Different shapes")
-        return None
-    repr, dim = data_wrapper.preferred_representation()
-    if repr != datatypes.DATA_REPRESENTATION_HISTOGRAM:
-        return None
-    values_1, bin_edges = data_wrapper_1.get_as_histogram()
-    values_2, bin_edges = data_wrapper_2.get_as_histogram()
-
-    norm_1 = values_1 / integral(data_wrapper_1)
-    norm_2 = values_2 / integral(data_wrapper_2)
-
+def _shape_numpy(norm_1, norm_2):
     try:
         return np.sum((norm_1 - norm_2)**2 / (norm_1 + norm_2)) / 2
     except ZeroDivisionError:
         METRICS_LOGGER.warning("Empty histograms, division by 0")
         return None
+        
+def _shape(data_wrapper_1, data_wrapper_2):
+    norm_1, norm_2 = (_norm(data_wrapper_1), norm(_data_wrapper_2))
+    return _shape_numpy(norm_1, norm_2)
+
+def shape(data_wrapper_1, data_wrapper_2):
+    if data_wrapper_1.get_shape() != data_wrapper_2.get_shape():
+        METRICS_LOGGER.warning("Different shapes")
+        return None
+    repr, dim = data_wrapper_1.preferred_representation()
+    if repr != datatypes.DATA_REPRESENTATION_HISTOGRAM:
+        return None
+    return _shape(data_wrapper_1, data_wrapper_2)
+
+
+
+
+def compute_metrics(data_wrapper_1, data_wrapper_2, [])
 
 def compute_metrics(data_wrappers, metrics_names, compare=False):
     if len(data_wrapper) < 2 and compare:
         METRICS_LOGGER.warning("Only one batch of data was passed, nothing to compare")
         return None
+    # first data in list is taken as reference
+    ref_data = data_wrappers[0]
+    # now do for all 
     for mn in metric_names:
         METRICS_LOGGER.info("Compare %s", mn)
     return None
